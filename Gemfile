b39748c5f53adf2d65d0a09cec9052b6c887a8f9
Mohd Khairi Mohd Adnan <khairi@labs.my>
2022-09-06 00:18:28 +0800

    Add tabler icon helper

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "inline_svg", "~> 1.8"
---
+++ app/helpers/icons_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module IconsHelper
+  def icon(filename, _text = nil, options = {})
+    InlineSvg::TransformPipeline.generate_html_from(read_tabler_svg(filename), options).html_safe
+  end
+
+  private
+
+  def icons_path
+    Rails.root.join('node_modules/@tabler/icons/icons/')
+  end
+
+  def read_tabler_svg(filename)
+    File.read("#{icons_path}/#{filename}.svg")
+  end
+end
