3ec43b14ff21dd7fd0c0bebcfadb49e22d7eafab
Mohd Khairi <khairi@labs.my>
2023-12-19 14:41:34 +0800

    Add toaster

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def flash_class(type)
+    color =
+      case type
+      when "notice"
+      "success"
+      when "alert", "error"
+      "danger"
+      else
+      "info"
+      end
+    klass = "text-bg-#{color} bg-#{color}"
+  end
 end
