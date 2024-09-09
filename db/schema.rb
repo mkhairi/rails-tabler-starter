2bd5bb135c229288af66caab0d516d583baef6a2
Mohd Khairi Mohd Adnan <khairi@labs.my>
2024-09-09 23:54:23 +0800

    Update package and db conf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_19_065221) do
+ActiveRecord::Schema[7.2].define(version: 2023_12_19_065221) do
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -20,5 +20,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_19_065221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
