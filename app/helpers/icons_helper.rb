08345a8f16ab834ea11ef1ec03e0689c663b62bd
Mohd Khairi <khairi@labs.my>
2024-09-10 18:05:26 +0800

    Update icons

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -1,18 +1,38 @@
 # frozen_string_literal: true
 
 module IconsHelper
-  def icon(filename, options = {})
-    icon_name = filename.to_s.dasherize
-    InlineSvg::TransformPipeline.generate_html_from(read_tabler_svg(icon_name), options).html_safe
+  def icon(name, text = nil, options = {})
+    if text.is_a?(Hash)
+      options = text
+      text = nil
+    end
+    options.reverse_merge!({ class: "" })
+    options["id"] = nil
+    options["class"] = options[:class]
+    options["width"] = "24px"
+    options["height"] = "24px"
+
+    if options.key?(:tooltip)
+      options["data-bs-toggle"] = "tooltip"
+      options["data-bs-placement"] = "top"
+      options["data-bs-title"] = options.delete(:tooltip)
+    end
+
+    style = options.delete(:filled) ? "filled" : "outline"
+
+    icon = InlineSvg::TransformPipeline.generate_html_from(read_tabler_svg(name.to_s, style), options)
+    return icon.html_safe if text.blank?
+
+    icon.html_safe + content_tag(:span, text, class: "ms-1")
   rescue StandardError
-    tag.span(icon_name) + content_tag(:span, icon_name, class: 'ml-1')
+    tag.span(name.to_s) + content_tag(:span, text, class: "ms-1 icon-not-found")
   end
 
-  def icons_path
-    Rails.public_path.join("assets/icons")
+  def icons_path(style)
+    Rails.public_path.join("assets/icons/#{style}")
   end
 
-  def read_tabler_svg(filename)
-    File.read("#{icons_path}/#{filename}.svg")
+  def read_tabler_svg(filename, style = "outline")
+    File.read("#{icons_path(style)}/#{filename.to_s.dasherize}.svg")
   end
 end
