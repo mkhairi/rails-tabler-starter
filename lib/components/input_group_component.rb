2fff436548e4bbdbcd8c1bc900e3d3c2e1a61bef
Mohd Khairi Mohd Adnan <khairi@labs.my>
2024-09-11 11:50:15 +0800

    Fix input group autoload

--- lib/components/input_group_component.rb
+++ lib/components/input_group_component.rb
@@ -1,13 +1,27 @@
+# frozen_string_literal: true
+
 # custom component requires input group wrapper
-module InputGroup
-  def prepend(wrapper_options = nil)
-    template.content_tag(:span, options[:prepend], class: "input-group-text")
+module InputGroupComponent
+  def prepend(args = nil)
+    if args[:icon_addon]
+      template.content_tag(:span, options[:prepend], class: "input-icon-addon")
+    else
+      template.content_tag(:span, options[:prepend], class: "input-group-text")
+    end
   end
 
-  def append(wrapper_options = nil)
-    template.content_tag(:span, options[:append], class: "input-group-text")
+  def append(args = nil)
+    if args[:icon_addon]
+      template.content_tag(:span, options[:append], class: "input-icon-addon")
+    else
+      template.content_tag(:span, options[:append], class: "input-group-text")
+    end
+  end
+
+  def text(args = nil)
+    template.content_tag(:span, args[:text], class: args[:class])
   end
 end
 
 # Register the component in Simple Form.
-SimpleForm.include_component(InputGroup)
+SimpleForm.include_component(InputGroupComponent)
