9eaf05f171f027fd83b374ed8da839d1072d6329
Mohd Khairi <khairi@labs.my>
2024-11-08 11:28:43 +0000

    Upgrade rails 8

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Page < ApplicationRecord
-  enum status: { draft: 0, published: 1, archived: 2 }, _prefix: true
+  enum :status, { draft: 0, published: 1, archived: 2 }, prefix: true
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :content, presence: true, length: { minimum: 10 }
